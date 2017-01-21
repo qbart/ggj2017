bda76f636e81e61a34195a2d141de3e265acca34
Jaroslaw Kisiolek <kisiolek.jaroslaw@gmail.com>
2017-01-21 18:40:10 +0100

    Add UI counters

---
+++ yarrr/Assets/Scripts/GameController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour {
+
+	public Text scoreCounterText;
+	public Text animalCounterText;
+	private int animalCount;
+	private int pointCount;
+	int interval = 1; 
+	float nextTime = 0;
+
+	// Use this for initialization
+	void Start () {
+		animalCount = 8;
+		pointCount = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if (Time.time >= nextTime) {
+			incrementPoints ();
+			nextTime += interval; 
+		}
+		updateCounters ();
+	}
+
+	void updateCounters(){
+		scoreCounterText.text = "" + pointCount;
+		animalCounterText.text = "" + animalCount;
+	}
+
+	void incrementPoints(){
+		pointCount += 10 * animalCount;
+	}
+}
