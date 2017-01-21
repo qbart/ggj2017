b0e657d54d8aeeb89b49be90bfbf4b759c02ac85
bart <wojtowicz.bartlomiej@gmail.com>
2017-01-21 03:45:30 +0100

    Update player

--- yarrr/Assets/Scripts/PlayerBoat.cs
+++ yarrr/Assets/Scripts/PlayerBoat.cs
@@ -13,15 +13,17 @@ class WaveFn
 
     public bool slidingDown(float x)
     {
-        float x1 = x - 0.05f;
-        float x2 = x + 0.05f;
+        float x1 = x - 0.1f;
+        float x2 = x + 0.1f;
         float y1 = f(x1);
         float y2 = f(x2);
-
-        return y2 < y1;
+        bool sliding = y2 < y1;
+        float epsilon = 0.05f;
+        bool allowSlope = Mathf.Abs(y1 - y2) >= epsilon;
+        return sliding && allowSlope;
     }
 
-    public Quaternion perfectRotation(float x)
+    public Quaternion perfectRotation(float x, float extraRotationAngle = 0)
     {
         float x1 = x - 0.05f;
         float x2 = x + 0.05f;
@@ -32,6 +34,10 @@ class WaveFn
         Vector2 waveChunk2 = new Vector2(x2, y2);
         Vector2 direction = (waveChunk2 - waveChunk1).normalized;
         Vector2 rotatedDirection = Quaternion.Euler(0, 0, 90) * direction;
+        if (extraRotationAngle != 0)
+        {
+            rotatedDirection = Quaternion.Euler(0, 0, extraRotationAngle) * rotatedDirection;
+        }
 
         return Quaternion.FromToRotation(Vector3.up, rotatedDirection);
     }
@@ -54,7 +60,7 @@ class WaveFn
             Debug.DrawLine(
                 new Vector2(drawX1, y1 - offset),
                 new Vector2(drawX2, y2 - offset),
-                Color.red
+                slidingDown(drawX1 + x) ? Color.yellow : Color.red
             );
         }
     }
@@ -67,6 +73,9 @@ public class PlayerBoat : MonoBehaviour
     float rotationForce = 30.0f;
     float adjustRotationConstantForce = 40.0f;
     float adjustRotationSlidingForce = 100.0f;
+    float maxAdjustRotationSlidingForce = 100.0f;
+    float maxBounceAngle = 20.0f;
+    float bounceAngle = 0;
     WaveFn fn;
     float curX;
     bool prevSlidingDown, slidingDown;
@@ -96,15 +105,27 @@ public class PlayerBoat : MonoBehaviour
         prevSlidingDown = slidingDown;
         slidingDown = fn.slidingDown(curX);
         if (!prevSlidingDown && slidingDown)
-            bounceSteps = 3;
+        {
+            adjustRotationSlidingForce = maxAdjustRotationSlidingForce;
+            bounceSteps = 5;
+            bounceAngle = -maxBounceAngle;
+        }
 
-        if (bounceSteps > 0)
+        if (bounceSteps > 0 || slidingDown)
         {
             Quaternion targetRotation = fn.perfectRotation(curX);
-            Quaternion currentRotation = transform.rotation;
+            targetRotation = fn.perfectRotation(curX, bounceAngle);
             Vector3 targetAngles = targetRotation.eulerAngles;
-            
-            Quaternion quat = targetRotation;
+            Vector3 currentAngles = transform.rotation.eulerAngles;
+            float angle = Mathf.Abs(targetAngles.z - currentAngles.z);
+            if (angle <= 2)
+            {
+                adjustRotationSlidingForce /= 2;
+                --bounceSteps;
+                bounceAngle = -bounceAngle / 2.0f;
+                if (bounceSteps == 1)
+                    bounceAngle = 0;
+            }
 
             Quaternion rotation = Quaternion.RotateTowards(
                 transform.rotation,
@@ -112,6 +133,7 @@ public class PlayerBoat : MonoBehaviour
                 adjustRotationSlidingForce * Time.deltaTime
             );
             transform.localRotation = rotation;
+            
         }
         //else
         //{
