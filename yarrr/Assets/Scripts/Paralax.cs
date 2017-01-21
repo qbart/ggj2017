8df8d78b2e5707d9405c5f452c9ffe86e5d026cd
Jaroslaw Kisiolek <kisiolek.jaroslaw@gmail.com>
2017-01-21 11:13:18 +0100

    Add paralax and clouds

---
+++ yarrr/Assets/Scripts/Paralax.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paralax : MonoBehaviour
+{
+	public float speed = -2.0f;
+	public GameObject[] objects;
+
+	private float singleItemWidth = 0;
+
+	void Start()
+	{
+		for (int i = 0; i < objects.Length; ++i)
+		{
+			objects[i].GetComponent<Rigidbody2D>().velocity = new Vector3(speed, 0, 0);
+		}
+
+		// all objects need to have the same size
+		singleItemWidth = objects[0].GetComponent<BoxCollider2D>().size.x;
+		Debug.Log (singleItemWidth);
+	}
+
+	public void onParalaxItemNeedsUpdate(GameObject item)
+	{
+		for (int i = 0; i < objects.Length; ++i)
+		{
+			if (objects[i].GetInstanceID() == item.GetInstanceID())
+			{
+				Vector3 oldPosition = objects[i].transform.position;
+				Vector3 newPosition = oldPosition;
+				newPosition.x += objects.Length * singleItemWidth;
+				objects[i].transform.position = newPosition;
+				break;
+			}
+		}
+
+	}
+}
---
+++ yarrr/Assets/Scripts/ParalaxItem.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParalaxItem : MonoBehaviour
+{
+	void Start()
+	{
+	}
+
+	void Update()
+	{
+
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		// if paralax item reached point where it should respawn (moved at the end)
+		if (other.CompareTag("RespawnTrigger"))
+		{
+			Paralax paralax = GetComponentInParent<Paralax>();
+			paralax.onParalaxItemNeedsUpdate(gameObject);
+		}
+	}
+}
