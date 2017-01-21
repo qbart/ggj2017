dbffa0d1a509fa2adaa13f5092907563aaeb62b0
Jaroslaw Kisiolek <kisiolek.jaroslaw@gmail.com>
2017-01-21 03:40:50 +0100

    Add harpoon cannon

---
+++ yarrr/Assets/Scripts/harpoon.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class harpoon : MonoBehaviour {
+	// Use this for initialization
+	public LayerMask animalLayer;
+
+	// Update is called once per frame
+	void Update () {
+		Vector3 mousePos = Input.mousePosition;
+
+		Vector3 objectPos = Camera.main.WorldToScreenPoint (transform.position);
+		mousePos.x = objectPos.x - mousePos.x;
+		mousePos.y = objectPos.y - mousePos.y;
+
+		float angle = Mathf.Atan2(mousePos.y, mousePos.x) * Mathf.Rad2Deg;
+		transform.rotation = Quaternion.Euler (new Vector3 (0, 0, angle)); 
+
+		if (Input.GetMouseButtonDown (0)) {
+			Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+			RaycastHit2D hit = Physics2D.Raycast(transform.position, mousePosition, 10, animalLayer);
+
+			Debug.DrawRay(transform.position, mousePosition, Color.red);
+			if(hit.collider != null){
+				hit.rigidbody.MovePosition(hit.rigidbody.position * Time.deltaTime);
+			}
+
+		}
+	}
+}
