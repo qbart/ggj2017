58574b0c522a07591d00187ea89799060c669edb
Jaroslaw Kisiolek <kisiolek.jaroslaw@gmail.com>
2017-01-22 13:28:05 +0100

    Flow

--- yarrr/Assets/Scripts/MenuController.cs
+++ yarrr/Assets/Scripts/MenuController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class GameOverController : MonoBehaviour
+public class MenuController : MonoBehaviour
 {
     public void onPlayClicked()
     {
